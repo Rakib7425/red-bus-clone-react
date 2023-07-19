2499f346951a1e204c32a5ce73b7c8ecee4f3aec
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-07-20 01:00:18 +0530

    modified:   src/contexts/GetBusses.jsx

--- src/contexts/GetBusses.jsx
+++ src/contexts/GetBusses.jsx
@@ -1,4 +1,5 @@
 
+// get data by searching
 const getBusses = async (source, destination) => {
     const url = `https://content.newtonschool.co/v1/pr/63b70222af4f30335b4b3b9a/buses?source=${source}&destination=${destination}`
     const res = await fetch(url);
