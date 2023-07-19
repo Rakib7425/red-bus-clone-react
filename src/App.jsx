46a616b6780fe2b61107db763339ee0cee22018b
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-07-19 17:56:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Header from './components/Header'
+import Home from './components/Home'
+import Results from './components/Results'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main className='bg-red-200 mx-auto'>
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+        <Results />
+      </main>
+    </>
+  )
+}
+
+export default App
